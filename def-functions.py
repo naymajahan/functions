a059d6ba761817b0528cae3be610b0d1449dacb3
naymajahan <naymajahanshoshe@gmail.com>
2021-03-21 17:13:37 +0600

    first commit

---
+++ def-functions.py
@@ -0,0 +1,28 @@
+#function defination
+#function Calling
+
+def f():
+    print("Hey Nayma")
+f()
+
+##Return Statement of Function
+
+def sum(a,b):
+    return a+b
+a = sum(3,4)
+print(a)
+
+##Function String Return
+
+def print_my_name(name):
+    print("your name is " +name)
+my_name=input()
+print_my_name(my_name)
+
+
+###None Value Return
+
+var = print("ok")
+print(type(var))
+
+
